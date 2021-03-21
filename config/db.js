52743e934e1d066422e7ea820c96efa73121f61a
Mauricio <mauricioresendez524@gmail.com>
2021-03-21 16:59:02 -0500

    Changing MongoDB URI name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { connect } = require("mongoose");
 
 const config = require('config');
-const db = config.get("mongoURI");
+const db = config.get("MONGO_URI");
 const mongoose = require('mongoose');
 
 mongoose.connect(db);
